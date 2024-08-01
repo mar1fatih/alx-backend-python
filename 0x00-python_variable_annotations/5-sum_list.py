d122e1bf50247d30fc41f3a0475d31ce12c6d5fe
mar1fatih <marouanefatih631@gmail.com>
2024-08-01 21:54:11 +0000

    task5

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""notated function"""
+from typing import List
+
+def sum_list(input_list: List[float]) -> float:
+    """sum_list function"""
+    c: float = 0
+    for item in input_list:
+        c += item
+    return c

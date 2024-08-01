4ddc90d23768f7f53c51414ac71731e58711d06f
mar1fatih <marouanefatih631@gmail.com>
2024-08-01 22:25:31 +0000

    task7

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""annotated function 7-to_kv.py"""
+from typing import Tuple, Union
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, Union[int, float]]:
+    """to_kv func"""
+    new_tuple: Tuple[str, Union[str, Union[int, float]]] = (k, v)
+    return new_tuple

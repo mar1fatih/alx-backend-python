97ebdc0b92dee9b4c4d0027fb2b4f2ab41478209
mar1fatih <marouanefatih631@gmail.com>
2024-08-06 22:57:45 +0000

    task 0

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+""" async function"""
+import asyncio
+import random
+import typing
+
+
+async def async_generator() -> typing.Generator[float, None, None]:
+    """yield a random number between 0 and 10 after waiting 1 second """
+    for i in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)

dd7a453d540172de20673538bf3219ee37ecbcf1
mar1fatih <marouanefatih631@gmail.com>
2024-08-26 23:35:52 +0100

    Mocking a property

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -20,3 +20,14 @@ class TestGithubOrgClient(unittest.TestCase):
         _return = org_client.org
         self.assertEqual(_return, patch.return_value)
         patch.assert_called_once
+
+    def test_public_repos_url(self):
+        """ testing GithubOrgClient._public_repos_url """
+        with patch.object(GithubOrgClient, "org",
+                          new_callable=PropertyMock) as mk:
+            mk.return_value = {"repos_url": "known payload"}
+            known_p = "known payload"
+            _client = GithubOrgClient(known_p)
+            _return = _client._public_repos_url
+            self.assertEqual(_return, known_p)
+            mk.assert_called_once
